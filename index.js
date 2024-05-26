0802cd728113baf5b880959b5e436f35aa63bea2
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-05-27 01:33:40 +0500

    end

--- index.js
+++ index.js
@@ -1,5 +1,172 @@
 // 1 masala
 
-for (let i = 1; i <=10; i++){
-    console.log(i);
-}
+// for (let i = 1; i <=10; i++){
+//     console.log(i);
+// }
+
+// 2 masala
+
+// for (let i = 100; i >=1; i--){
+//     console.log(i);
+// }
+
+// 3 masala
+
+// let sum  = 0;
+// for (let i = 2; i <=50; i++){
+//     (sum=sum+i);
+// } console.log(sum);
+
+// 4-masala
+
+// let sum = 0;
+// let n = 100;
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// 5 masala
+
+// let sum = 0;
+// let n = 100;
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 1) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// 6 masala
+
+// let sum =0;
+// let n = 10;
+// for (let i = 1; i <= n; i++) {
+//     if (n % i == 0){
+//         console.log (i);
+//     }
+// }
+
+// 7 masala
+
+// let sum =0;
+// let counter = 0;
+// let n = 10;
+// for (let i = 1; i <= n; i++) {
+//     if (n % i == 0){
+//         counter=counter+1;
+//         sum = sum +i;
+//     }
+// } console.log (sum);
+// console.log (counter);
+
+// 8 masala
+
+
+// for 1
+
+// let k = +prompt("k");
+// let n =+prompt ("n");
+// for(let i =1; i<=n; i++){
+//     console.log(k);
+// }
+
+// for2
+
+// let a = +prompt;
+// let b = +prompt;
+// for (let i = a; i <= b; i++ ){
+//     console.log(i);
+// }
+
+
+// for3
+
+// let a = +prompt ("a");
+// let b = +prompt ("b");
+// for (let i = b; i <= a; i-- ){
+//     console.log(i);
+// }
+
+// for4
+
+// let m = +prompt ("m") ;
+// let res = 0;
+
+// for(let i =1; i<=10; i++){
+//     res=i*m;
+//     console.log(i*m);
+// }
+
+// for5
+
+// let m = +prompt ("m") ;
+// let res = 0;
+
+// for(let i =0.1; i<=10; i++){
+//     res=i*m;
+//     console.log(i*m);
+// }
+
+// for6
+
+
+// let m = +prompt ("m") ;
+// let res = 0;
+
+// for(let i =0.2; i<=10; i++){
+//     res=i*m;
+//     console.log(i*m);
+// }
+
+
+// for7
+
+// let a = +prompt ("a");
+// let b = +prompt ("b");
+// sum = 0;
+// for (let i = a; i <= b; i++ ){
+//     sum=sum+i;
+// } console.log (sum);
+
+
+// for8
+
+// let a = +prompt ("a");
+// let b = +prompt ("b");
+// p = 1;
+// for (let i = a; i <= b; i++ ){
+//     p=p*i;
+// } console.log (p);
+
+// for9
+
+// let a = +prompt ("a");
+// let b = +prompt ("b");
+// p = 1;
+// for (let i = a; i <= b; i++ ){
+//     p=p*i*i;
+// } console.log (p);
+
+// for10
+
+// let n = +prompt("n");
+// let sum=0;
+// for(let i = 1; i <= n; i++){
+//     sum=sum+1/i;
+//     console.log(sum);
+// }
+
+// for11
+
+// let n = +prompt("n");
+// let sum=0;
+// for(let i = 1; i <= n; i++){
+//     sum= (n+i)*2;
+//     console.log(sum);
+// }
+
+
+// for12
